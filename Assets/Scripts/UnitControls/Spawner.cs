1ad0e35e870a3ae16ee53ac0f91c434488cf1e55
RichardAOlive <54379466+RichardAOlive@users.noreply.github.com>
2022-04-19 09:26:35 -0500

    Added Modeling PDFs
    Added Modeling PDFs

--- Assets/Scripts/UnitControls/Spawner.cs
+++ Assets/Scripts/UnitControls/Spawner.cs
@@ -15,13 +15,13 @@ public class Spawner : MonoBehaviour
         Vector3 spawnpoint = transform.position;
         Vector3 spawnoffset = spawnpoint + new Vector3(5f, 0, 0);
         
+    
         Instantiate(Prefab, spawnoffset, transform.rotation);
     }
 
     // Update is called once per frame
     public void OnMouseDown()
     {
-    
 
         SpawnUnit();
     }
